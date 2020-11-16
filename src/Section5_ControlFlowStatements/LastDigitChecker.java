85c7dee44254764ad172ee6c1da560424e7a823a
Peggy <peggy22509385@gmail.com>
2020-11-16 22:14:44 +0100

    Java Exercises

---
+++ src/Section5_ControlFlowStatements/LastDigitChecker.java
@@ -0,0 +1,41 @@
+package Section5_ControlFlowStatements;
+
+/*Write a method named hasSameLastDigit with three parameters of type int.
+Each number should be within the range of 10 (inclusive) - 1000 (inclusive). If one of the numbers is not within the range, the method should return false.
+The method should return true if at least two of the numbers share the same rightmost digit; otherwise, it should return false.
+
+EXAMPLE INPUT/OUTPUT:
+* hasSameLastDigit (41, 22, 71); → should return true since 1 is the rightmost digit in numbers 41 and 71
+* hasSameLastDigit (23, 32, 42); → should return true since 2 is the rightmost digit in numbers 32 and 42
+* hasSameLastDigit (9, 99, 999); → should return false since 9 is not within the range of 10-1000
+
+Write another method named isValid with one parameter of type int.
+The method needs to return true if the number parameter is in range of 10(inclusive) - 1000(inclusive), otherwise return false.
+
+EXAMPLE INPUT/OUTPUT
+* isValid(10); → should return true since 10 is within the range of 10-1000
+* isValid(468); → should return true since 468 is within the range of 10-1000
+* isValid(1051); → should return false since 1051 is not within the range of 10-1000
+
+NOTE: All methods need to be defined as public static as we have been doing so far in the course.
+
+NOTE: Do not add a main method to the solution code.*/
+public class LastDigitChecker {
+    public static void main(String[] args) {
+        System.out.println("Is there the same rightmost digit in them ? " + hasSameLastDigit(41, 22, 71));
+        System.out.println("Is there the same rightmost digit in them ? " + hasSameLastDigit(23, 32, 42));
+        System.out.println("Is there the same rightmost digit in them ? " + hasSameLastDigit(9, 99, 999));
+    }
+
+    public static boolean hasSameLastDigit(int n1, int n2, int n3) {
+
+        if (isValid(n1) && isValid(n2) && isValid(n3)) {
+            return ((n1 %= 10) == (n2 %= 10)) || (n1 == (n3 %= 10)) || ((n2 %= 10) == (n3 %= 10));
+        }
+        return false;
+    }
+
+    public static boolean isValid(int number) {
+        return number >= 10 && number <= 1000;
+    }
+}
